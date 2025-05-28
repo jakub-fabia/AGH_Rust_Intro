eea241e8284712f3498ecc554881dd80d80f5000
Jakub Fabia <jakub.fabia@proton.me>
2025-05-28 17:40:01 +0200

    Working saving to json

--- src/cli-weather/src/main.rs
+++ src/cli-weather/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let api_key = env::var("WEATHER_API_KEY").expect("Brak WEATHER_API_KEY");
     let city = "Cracow"; // później zmienimy na parametr CLI
 
-    match weather::fetch_weather(&api_key, city) {
+    match weather::fetch_weather_from_file(&api_key, city) {
         Ok(data) => {
             println!("🌤 Pogoda teraz: {}°C, {}", data.current.temp_c, data.current.condition.text);
             println!("💨 PM2.5: {}, PM10: {}", data.current.air_quality.pm2_5, data.current.air_quality.pm10);
--- src/cli-weather/src/weather.rs
+++ src/cli-weather/src/weather.rs
@@ -1,24 +1,27 @@
 use reqwest::blocking::get;
 use serde::Deserialize;
+use serde::Serialize;
+use std::fs::File;
+use std::io::Write;
+use std::io::Read;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct WeatherData {
     pub location: Location,
     pub current: Current,
     pub forecast: Forecast,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Location {
     pub name: String,
     pub country: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Current {
     pub last_updated: String,
     pub temp_c: f64,
-    pub feelslike_c: f64,
     pub is_day: u8,
     pub wind_kph: f64,
     pub wind_dir: String,
@@ -27,12 +30,12 @@ pub struct Current {
     pub air_quality: AirQuality,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Condition {
     pub text: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct AirQuality {
     #[serde(rename = "pm2_5")]
     pub pm2_5: f64,
@@ -40,28 +43,36 @@ pub struct AirQuality {
     pub pm10: f64,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Forecast {
     pub forecastday: Vec<ForecastDay>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct ForecastDay {
     pub date: String,
     pub day: Day,
     pub hour: Vec<HourData>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Day {
+    pub maxtemp_c: f64,
     pub avgtemp_c: f64,
+    pub mintemp_c: f64,
+    pub maxwind_kph: f64,
+    pub totalprecip_mm: f64,
+    pub daily_chance_of_rain: u8,
     pub condition: Condition,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct HourData {
     pub time: String,
     pub temp_c: f64,
+    pub wind_kph: f64,
+    pub humidity: u8,
+    pub chance_of_rain: u8,
     pub condition: Condition,
 }
 
@@ -72,5 +83,17 @@ pub fn fetch_weather(api_key: &str, city: &str) -> Result<WeatherData, Box<dyn s
     );
 
     let resp = get(&url)?.json::<WeatherData>()?;
+
+    if let Ok(json_str) = serde_json::to_string_pretty(&resp) {
+        let _ = File::create("weather_dump.json").and_then(|mut f| f.write_all(json_str.as_bytes()));
+    }
     Ok(resp)
+
+    // let mut file = File::open("weather_dump.json")?;
+    // let mut contents = String::new();
+    // file.read_to_string(&mut contents)?;
+
+    // let data: WeatherData = serde_json::from_str(&contents)?;
+
+    // Ok(data)
 }
